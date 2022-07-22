627b2ee62db80a1165c65ec37939b0136c8fd505
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2022-07-22 13:24:22 +0200

    🥇 First commit

---
+++ src/make.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import glob
+import json
+
+cells = []
+
+for source_file in glob.glob("*.txt"):
+  for snippet in open(source_file).read().split("@@@"):
+    snippet = snippet.strip()
+    if not snippet:
+      continue
+    cells.append(dict(
+      cell_type="code",
+      metadata=dict(),
+      source=snippet,
+    ))
+  notebook = dict(
+    nbformat=4,
+    nbformat_minor=0,
+    metadata=dict(
+      kernelspec=dict(name="python3", display_name="Python 3"),
+      language_info=dict(name="python"),
+    ),
+    cells=cells
+  )
+  destination_file = "../work/" + source_file[:-4] + ".ipynb"
+  with open(destination_file, "w") as f:
+    json.dump(notebook, f)
---
+++ sync.sh
@@ -0,0 +1,2 @@
+rsync -av --exclude work/.ipynb_checkpoints/ \
+  ./ jupyter@185.145.250.49:jupyter/
